4e9ea8fc6b2e9a5f3427711915293ce9a61d7e72
gingeralesy <gingeralesy@gmail.com>
2017-10-15 15:13:15 +0300

    Added wireframe mode

--- src/handlers.c
+++ src/handlers.c
@@ -4,6 +4,10 @@ void handle_input(GLFWwindow *window)
 {
   if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
     glfwSetWindowShouldClose(window, GL_TRUE);
+  if (glfwGetKey(window, GLFW_KEY_1) == GLFW_PRESS)
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+  if (glfwGetKey(window, GLFW_KEY_2) == GLFW_PRESS)
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 }
 
 void handle_resize(GLFWwindow *window, GLint width, GLint height)
